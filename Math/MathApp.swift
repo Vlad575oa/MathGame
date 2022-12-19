f66fbc179c505cc56d4cd96be5a50df13386771b
Vlad <vlad14196@gmail.com>
2022-12-19 12:01:10 +0300

    Initial Commit

---
+++ Math/MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathApp.swift
+//  Math
+//
+//  Created by user on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
